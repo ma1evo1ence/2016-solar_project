3aa19714788735dc4d5fdc9974508b3d6fcc8fb6
nastovikova <asn0602@mail.ru>
2019-11-14 18:52:31 +0300

    step 3

--- solar_input.py
+++ solar_input.py
@@ -23,6 +23,10 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            if object_type == "planet":  # FIXME: do the same for planet
+                planet = Planet()
+                objects.append(planet)
+                parse_planet_parameters(line, planet)
             else:
                 print("Unknown space object")
 
@@ -92,7 +96,7 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            print(obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy, file=out_file)
             # FIXME: should store real values
 
 # FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
